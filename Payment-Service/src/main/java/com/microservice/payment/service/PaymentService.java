4d007fe0829e1a4fae9f53a9766ebfeec4f6af67
Soumya Mukherjee <rivu10@gmail.com>
2021-11-07 13:24:45 +0530

    Payment service related changes

--- Payment-Service/src/main/java/com/microservice/payment/service/PaymentService.java
+++ Payment-Service/src/main/java/com/microservice/payment/service/PaymentService.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
 import com.microservice.payment.common.Order;
 import com.microservice.payment.common.PaymentResponse;
 import com.microservice.payment.entity.Payment;
